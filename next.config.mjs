474785b5987cf1c7e404b398f5bc0773b3aef125
ryosuke-horie <r_horie@sincereed.com>
2025-02-14 20:31:42 +0900

    feat: edgeランタイムを追加し、Cloudflareの開発プラットフォームを設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 import path from 'path';
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
 };
+
+if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+}
+
 export default nextConfig;
